90370b7a350882bae759fda293ede7b6b9080093
lyndexer <mckinney.m93@gmail.com>
2020-10-28 16:25:50 -0700

    Cors issues, added cors to api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 let indexRouter = require('./routes/index');
 let adminRouter = require('./routes/admin');
@@ -28,6 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
